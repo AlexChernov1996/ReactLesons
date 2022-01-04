36b1088e1846db553a188f582773a9b381cf66e1
Alexandr Chernov <vonrehcxela@gmail.com>
2022-01-04 23:06:53 +0200

    Components.Props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Rating} from "./components/Rating/Rating";
+import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>This is App</h1>
+            <Rating value ={1}/>
+            <Rating value ={2}/>
+            <Rating value ={3}/>
+            <Rating value ={4}/>
+            <Rating value ={5}/>
+            <Accordion title = {"Accordion1"}/>
+            <Accordion title = {"Accordion2"}/>
+        </div>
+    );
 }
+
+
+
+
+
+
 
 export default App;
